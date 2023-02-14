7848461ba1499fb1f8b87a9e7261c32196f2d502
lonelil <51315646+lonelil@users.noreply.github.com>
2023-02-15 01:21:38 +0800

    added webhook support

--- src/daily.ts
+++ src/daily.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import { Client, Language } from "genshin-kit.js";
+import { Webhook, MessageBuilder } from "discord-webhook-node";
 const accounts = JSON.parse(process.env.HOYO_COOKIES as string);
 console.log(`Found ${accounts.length} accounts!`);
 
@@ -12,15 +13,42 @@ accounts.forEach(
 
     client.login(account.ltuid, account.ltoken);
     if (client.isLogin()) {
-      console.warn(`Account ${i + 1} |`, `Logged In.`);
+      console.log(`Account ${i + 1} |`, `Logged In.`);
+
       const getDaily = await client.dailyReward.checkIn();
       if (getDaily.code === 0) {
         console.log(
           `Account ${i + 1} | ` +
-            `Redeemed ${getDaily.rewards?.name}!`
+            `Redeemed ${getDaily.rewards?.count} ${getDaily.rewards?.name}!`
         );
+
+        if (process.env.DISCORD_WEBHOOK) {
+          const hook = new Webhook(process.env.DISCORD_WEBHOOK);
+          hook.setUsername(`AnimeLabs-Automation | Account ${i + 1}`);
+          const embed = new MessageBuilder()
+            .setTitle(
+              `Redeemed ${getDaily.rewards?.count} ${getDaily.rewards?.name}!`
+            )
+            .setThumbnail(getDaily.rewards?.icon as string)
+            .setDescription(
+              `Successfully redeemed at <t:${Math.round(Date.now() / 1000)}:R>!`
+            )
+            .setFooter("AnimeLabs-Automation by lonelil")
+            .setTimestamp();
+          hook.send(embed);
+        }
       } else {
-        console.warn(`Account ${i + 1} |`, getDaily);
+        console.error(`Account ${i + 1} | An error has occurred |`, getDaily);
+        if (process.env.DISCORD_WEBHOOK) {
+          const hook = new Webhook(process.env.DISCORD_WEBHOOK);
+          hook.setUsername(`AnimeLabs-Automation | Account ${i + 1}`);
+          const embed = new MessageBuilder()
+            .setTitle("An error has occurred!")
+            .setDescription(JSON.stringify(getDaily))
+            .setFooter("AnimeLabs-Automation by lonelil")
+            .setTimestamp();
+          hook.send(embed);
+        }
       }
     }
   }
